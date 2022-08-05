07040830dc06a2804edce1b6acb62f91f5f517f6
MrMelog <87619107+MrMelog@users.noreply.github.com>
2022-08-05 23:04:05 +0300

    1.1

---
+++ Render Prj/Render Prj/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Render_Prj
+{
+    
+    class RenderObj
+    {
+        private string _fullname = "";
+        private Int64 _lenght;
+        private string _extension = "";
+        private string _name = "";
+
+        public FileInfo _file;
+
+        public void Inform()
+        {
+            _lenght = _file.Length;
+            _fullname = _file.FullName;
+            _extension = _file.Extension;
+            _name = _file.Name;
+        }
+
+        public void Printer()
+        {
+            Console.Write(
+                "Name: {0} \n" +
+                "Path: {1} \n" +
+                "Format: {2} \n" +
+                "Size: {3} byte" , _name, _fullname , _extension , _lenght );
+        }
+
+        public void FilInf(FileInfo inf)
+        {
+            _file = inf;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(@"Enter the Path(like: C:\Users\tomfitz )");
+            //string path = Console.ReadLine();
+            DirectoryInfo direct;
+            FileInfo[] dirF;
+            while (true) 
+            {
+                try
+                {
+                    string path = @Console.ReadLine();
+                    direct = new DirectoryInfo(path);
+                    dirF = direct.GetFiles();
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Pls try again.");
+                }
+            }
+            if (dirF.Length > 0)
+            { 
+                RenderObj[] Files = new RenderObj[dirF.Length];
+                int i = 0;
+                foreach (var fi in dirF)
+                {
+                    Console.WriteLine("{0}- {1}",i,fi);
+                    Files[i] = new RenderObj();
+                    Files[i]._file = fi;
+                    i++;
+                }
+                Console.WriteLine("Enter the number of file.");
+                var try_i = Console.ReadLine();
+                int num = -1;
+                while (!((int.TryParse(try_i, out num)) && num >= 0 && num <= dirF.Length-1))
+                {
+                    Console.WriteLine("Pls try again.");
+                    try_i = Console.ReadLine();
+                }
+                Files[num].Inform();
+                Files[num].Printer();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Render Prj/Render Prj/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Render Prj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Render Prj")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("4e128424-dc39-4e41-859d-46255c428fa9")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
